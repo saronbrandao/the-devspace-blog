122373772596b3cb53bfef5ff0ca60f981a79823
Saron <saronbrandao@gmail.com>
2023-05-12 16:44:58 +0200

    updates

--- pages/api/search.js
+++ pages/api/search.js
@@ -7,8 +7,7 @@ const search = (req, res) => {
 
   if (process.env.NODE_ENV === 'production') {
     // Fetch from cache
-    // posts = require('../../cache/data').posts;
-    return;
+    posts = require('../../cache/data').posts;
   } else {
     const files = fs.readdirSync(path.join('posts'));
 
